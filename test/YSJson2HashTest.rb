17b0afe0b2da693aca5bf07a41673dd7adcfba44
YumaYX <akasatana.y@gmail.com>
2022-04-23 02:36:27 +0900

    add Json to Hash class

---
+++ test/YSJson2HashTest.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require './lib/YSJson2Hash'
+
+class YSJson2HashTest < Minitest::Test
+
+  def test_json2hash
+    h1 = YSJson2Hash.new('./test/data/YSSampleData.json')
+    assert_equal({"key"=>"value"}, h1.json_hash)
+  end
+end
