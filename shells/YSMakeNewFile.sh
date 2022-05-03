edc78e5241ebfbced9487eed78517d1853e9a6af
YumaYX <akasatana.y@gmail.com>
2022-05-04 02:01:18 +0900

    add preparing file shell

---
+++ shells/YSMakeNewFile.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# sh shells/YSMakeNewFile.sh FileName
+
+if [ ${#} -ne 1 ]; then
+	echo "ERROR: Argument Error\nTerminated."; exit 254
+fi
+
+filename="${1%.rb}"
+filepath="./lib/${filename}.rb"
+
+if [ -e "${filepath}" ]; then
+  echo "File exists.\nTerminated."; exit 255
+fi
+
+touch "${filepath}"
+
+test_filename="${filename}Test.rb"
+
+cat <<EOF > ./test/"${test_filename}"
+require 'minitest/autorun'
+require './lib/${filename}'
+
+class ${test_filename%.rb} < Minitest::Test
+
+end
+EOF
+
+ls -1 ./*/*${filename}*
