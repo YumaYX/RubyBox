eddb5a5369a264dce585dc3d429b187ac109a8b1
YumaYX <akasatana.y@gmail.com>
2022-07-06 00:15:05 +0900

    refactor: update

--- lib/YSPrepareController.rb
+++ lib/YSPrepareController.rb
@@ -12,7 +12,7 @@ end
 
 controller_name = File.basename(ARGV[0].chomp, '.*')
 Y3.info "making controller for #{controller_name}"
-Y3.info "touch lib/#{controller_name}.rb"
+Y3.info "touch #{Y3::LIB_DIR}/#{controller_name}.rb"
 
 if File.exist?(controller_name + '.rb')
   Y3.banner 'ERROR:'
@@ -20,21 +20,19 @@ if File.exist?(controller_name + '.rb')
   exit 255
 end
 
-template_dir = 'lib/template/'
-
 ['data', 'op'].each do |sub_directory|
   new_dir = sub_directory + '/' + controller_name
   Y3.info "mkdir #{new_dir}"
   FileUtils.mkdir_p(new_dir)
 
   if sub_directory.eql?('data') then
-    config = ERB.new(File.read(template_dir + '/YSConfig.json.erb' )).result(binding)
+    config = ERB.new(File.read(Y3::TEMPLATE_DIR + '/YSConfig.json.erb' )).result(binding)
     Y3.file_writer(new_dir + '/config.json' ,config)
   end
 end
 
 
-controller = ERB.new(File.read(template_dir + '/YSTemplate.rb.erb' )).result(binding)
-Y3.file_writer("./lib/#{controller_name}.rb" ,controller)
+controller = ERB.new(File.read(Y3::TEMPLATE_DIR + '/YSTemplate.rb.erb' )).result(binding)
+Y3.file_writer(Y3::LIB_DIR + "/#{controller_name}.rb" ,controller)
 
 exit 0
--- lib/YSPrepareLib.rb
+++ lib/YSPrepareLib.rb
@@ -11,18 +11,16 @@ end
 lib_name = File.basename(ARGV[0].chomp, '.*')
 Y3.info "making lib and test files for #{lib_name}"
 
-if File.exist?('./lib/' + lib_name + '.rb')
+if File.exist?(Y3::LIB_DIR + '/' + lib_name + '.rb')
   Y3.banner 'ERROR:'
-  Y3.banner './lib/' + "#{lib_name}.rb is already exist."
+  Y3.banner Y3::LIB_DIR + "/#{lib_name}.rb is already exist."
   exit 255
 end
 
-template_dir = 'lib/template/'
+test_file = ERB.new(File.read(Y3::TEMPLATE_DIR + '/YSTestTemplate.rb.erb' )).result(binding)
+Y3.file_writer(Y3::TEST_DIR + "/#{lib_name}Test.rb", test_file)
 
-test_file = ERB.new(File.read(template_dir + '/YSTestTemplate.rb.erb' )).result(binding)
-Y3.file_writer("./test/#{lib_name}Test.rb" ,test_file)
-
-Y3.file_writer("./lib/#{lib_name}.rb" ,'')
+Y3.file_writer(Y3::LIB_DIR + "/#{lib_name}.rb", '')
 
 
 exit 0
